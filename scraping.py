c9d28344204d5010b3cdfedb6a05228bd6c12c87
abhee238 <abhiram.vattikonda@gmail.com>
2024-12-26 13:22:52 +0530

    scraping initailized in folder

--- scraping.py
+++ scraping.py
@@ -2,13 +2,12 @@ import requests
 from bs4 import BeautifulSoup as bs
 import re, json
 
-
+USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
 
     
 
 def get_name_and_links():
     URL='https://www.imdb.com/chart/top/?ref_=nv_mv_250'
-    USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
     headers = {"user-agent": USER_AGENT} # adding the user agent
     resp = requests.get(URL, headers=headers)
     soup = bs(resp.content, "html.parser")
@@ -22,11 +21,17 @@ def get_name_and_links():
 
     return names
 
+def get_directors(name_and_links):
+    for i in name_and_links:
+        headers = {"user-agent": USER_AGENT} # adding the user agent
+        resp = requests.get(name_and_links[i], headers=headers)
+        soup = bs(resp.content, "html.parser")
+        data = json.loads(soup.find('script', attrs={'type': "application/ld+json"}).text)
+
 
 def main():
     name_and_links = get_name_and_links()
-
-
+    directors = get_directors(name_and_links)
 
 # Example usage
 if __name__ == "__main__":
