71eb73bf90bf0b446c1a11255e8f0e45a869ff72
abhee238 <abhiram.vattikonda@gmail.com>
2025-01-10 13:06:43 +0530

    animation click

--- animations/scraping.py
+++ animations/scraping.py
@@ -15,6 +15,7 @@ def main():
     count =  int(driver.find_element(by=By.CSS_SELECTOR, value=".sc-13add9d7-3.fwjHEn").text.split()[2].replace(",", ""))
     
     movies = []
+    last_movie_index = 0
     iteration = 0
     wait = WebDriverWait(driver, 10)
     short_wait = WebDriverWait(driver, 5)
@@ -23,11 +24,22 @@ def main():
         if iteration > (count / 50) + 1:
             raise RuntimeError("too many iterations")
 
-        displayed_movies = wait.
+        displayed_movies = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//li[contains(@class, 'ipc-metadata-list-summary-item')]")))
 
-        pass
+        for i in range(last_movie_index, len(displayed_movies)):
+            el = wait.until(EC.visibility_of_element_located((By.XPATH, f"(//li[contains(@class, 'ipc-metadata-list-summary-item')])[{i}]")))
+            pass
 
+        last_movie_index = len(movies)
+        if last_movie_index != count:
+            try:
+                showmore = wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[contains(@class, 'ipc-see-more__button')]")))
+                driver.execute_script("arguments[0].click();", showmore)
+                
 
+        iteration += 1
+
+    print(last_movie_index)
     print("--- %s seconds ---" % (time.time() - start_time))
     
 
