69564bd78c6032f9a839a173b74d9287b38368c3
abhee238 <abhiram.vattikonda@gmail.com>
2025-01-10 12:29:22 +0530

    animation

--- animations/scraping.py
+++ animations/scraping.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup as bs
 import time
 
@@ -11,12 +13,20 @@ def main():
     driver.get("https://www.imdb.com/search/title/?title_type=feature&genres=animation&countries=US&languages=en")
     driver.implicitly_wait(10)
     count =  int(driver.find_element(by=By.CSS_SELECTOR, value=".sc-13add9d7-3.fwjHEn").text.split()[2].replace(",", ""))
-    driver.implicitly_wait(10)
-    buttons = driver.find_element(by=By.CLASS_NAME, value="ipc-see-more__text")
-    click=driver.execute_script("arguments[0].click();",buttons)
-   # while buttons.tag_name != "button":
-    #    buttons = buttons.find_element(By.XPATH, "..")
-    #buttons.click()
+    
+    movies = []
+    iteration = 0
+    wait = WebDriverWait(driver, 10)
+    short_wait = WebDriverWait(driver, 5)
+
+    while len(movies) != count:
+        if iteration > (count / 50) + 1:
+            raise RuntimeError("too many iterations")
+
+        displayed_movies = wait.
+
+        pass
+
 
     print("--- %s seconds ---" % (time.time() - start_time))
     
