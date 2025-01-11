1c838ce0712785a1f7094941cb7f41a6abfa79b1
abhee238 <abhiram.vattikonda@gmail.com>
2025-01-12 01:07:56 +0530

    works but there are issues

--- animations/scraping.py
+++ animations/scraping.py
@@ -14,7 +14,7 @@ def main():
     driver.implicitly_wait(10)
     count =  int(driver.find_element(by=By.CSS_SELECTOR, value=".sc-13add9d7-3.fwjHEn").text.split()[2].replace(",", ""))
     
-    movies = []
+    movies = {}
     last_movie_index = 0
     iteration = 0
     wait = WebDriverWait(driver, 10)
@@ -27,19 +27,32 @@ def main():
         displayed_movies = wait.until(EC.visibility_of_all_elements_located((By.XPATH, "//li[contains(@class, 'ipc-metadata-list-summary-item')]")))
 
         for i in range(last_movie_index, len(displayed_movies)):
-            el = wait.until(EC.visibility_of_element_located((By.XPATH, f"(//li[contains(@class, 'ipc-metadata-list-summary-item')])[{i}]")))
-            pass
+            el = wait.until(EC.visibility_of_element_located((By.XPATH, f"(//li[contains(@class, 'ipc-metadata-list-summary-item')])[{i+1}]")))
+        
+            name = el.find_element(By.XPATH, './/h3').text.split()[1]
+            link = el.find_element(By.XPATH, ".//a").get_attribute("href")
+            movies.update({name : link})
+            print({name : link})
+            print("Collected:", name)
 
         last_movie_index = len(movies)
         if last_movie_index != count:
             try:
                 showmore = wait.until(EC.element_to_be_clickable((By.XPATH, f"//button[contains(@class, 'ipc-see-more__button')]")))
                 driver.execute_script("arguments[0].click();", showmore)
-                
+                wait.until(EC.invisibility_of_element_located((By.XPATH, f"//button[contains(@class, 'ipc-see-more__button') and @disabled]")))
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            except TimeoutError:
+                pass
 
+        break
         iteration += 1
 
     print(last_movie_index)
+
+    with open("animation.txt", "w") as file:
+        for movie in movies:
+            file.write(f"{movie} : {movies[movie]}\n")
     print("--- %s seconds ---" % (time.time() - start_time))
     
 
