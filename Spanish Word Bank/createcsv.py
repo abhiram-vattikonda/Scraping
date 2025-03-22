dece7aed3accdb4cbfa7c4441b9b9a13d0e20f8b
abhee238 <abhiram.vattikonda@gmail.com>
2025-03-23 03:09:48 +0530

    nice

--- Spanish Word Bank/createcsv.py
+++ Spanish Word Bank/createcsv.py
@@ -1,5 +1,6 @@
 import csv, requests, re, time
 from bs4 import BeautifulSoup as bs
+import lxml
 
 
 words_missed = []
@@ -27,14 +28,14 @@ def main():
             for word in file_as_list[count:]:
                 word = word.strip()
 
-                html = requests.get("https://www.spanishdict.com/translate/" + word.strip() + "?langFrom=es").text
-                soup = bs(html, 'html.parser')
+                html = requests.get("https://www.spanishdict.com/translate/" + word + "?langFrom=es").text
+                soup = bs(html, 'lxml')
 
                 if word in not_words:
                     continue
 
                 try:
-                    translation, parents = getTranslation(soup, word)
+                    translation, parents = getTranslation(soup)
                 except:
                     error_words.append(word)
                     print("Error with a translation or parents")
@@ -61,48 +62,71 @@ def main():
                 if count % 500 == 0:
                     print(count)
 
+                if count == 50:
+                    break
+
 
     print("--- %s seconds ---" % (time.time() - start_time))
     print(words_missed)
     print(error_words)
 
 
-def getTranslation(soup : bs, word : str):
+def getTranslation(soup : bs):
 
     yellowBox = soup.find(attrs={"class" : "AF8dOcYf"})
 
     if yellowBox:
-        #print("Went into yellow box for", word)
-        meaning_for_nonverbs = soup.find_all(attrs={"class" : "le4jo4Ji"})
-        meaning_for_verbs = soup.find_all(attrs={"class" : "iG5azK28"})
-        translation = ""
-    
-        for num, sentance in enumerate(meaning_for_verbs):
-            s = sentance.find_all("span")
-            sentance = "".join([i.text + " " for i in s[:-1]])
-            #print(sentance)
-            try:
-                if sentance.find("conjugation") != -1:
-                    translation += sentance.strip() + ", \n"
-                else:
-                    translation += (meaning_for_nonverbs[num].text).split('-')[1] + ", \n"
-            except IndexError:
-                print("Error in translation")
-        
-        parent = soup.find_all(lambda tag: tag.has_attr("class") and tag["class"] == ["wpZ6GBd8"])
-        parents = ""
-        for x in parent:
-            try:
-                parents += (x.text) + ", "
-            except IndexError:
-                print("Error in finding parents")
-
-        #print(translation[:-2]+ "  |  " + parents[:-2])
-        return (translation[:-2], parents[:-2])
+        return getStuffFromYellowBox(soup)
 
     else:
-        word_and_meaning = soup.find_all(attrs={"class" : "YZ8gKxpm"})
-        return ("".join([(x.text) + ", " for x in word_and_meaning[1:]])[:-2], "")
+        return getStuffFromDefault(soup)
+    
+
+def getStuffFromDefault(soup: bs):
+    word_and_meaning = soup.find_all(attrs={"class" : "YZ8gKxpm"})
+    return ("".join([(x.text) + ", " for x in word_and_meaning[1:]])[:-2], "")
+
+
+def getStuffFromYellowBox(soup: bs):
+    translation = getTranslationInYelloBox(soup)
+    parents = getParentsInYellowBox(soup)
+
+    #print(translation+ "  |  " + parents)
+    return (translation, parents)
+
+
+def getTranslationInYelloBox(soup : bs):
+    #print("Went into yellow box for", word)
+    meaning_for_nonverbs = soup.find_all(attrs={"class" : "le4jo4Ji"})
+    meaning_for_verbs = soup.find_all(attrs={"class" : "iG5azK28"})
+    translation = ""
+
+    for num, sentance in enumerate(meaning_for_verbs):
+        # to get the conjugation part
+        s = sentance.find_all("span")
+        sentance = "".join([i.text + " " for i in s[:-1]])
+        #print(sentance)
+        try:
+            if sentance.find("conjugation") != -1:
+                translation += sentance.strip() + ", \n"
+            else:
+                translation += (meaning_for_nonverbs[num].text).split('-')[1] + ", \n"
+        except IndexError:
+            print("Error in translation")
+
+    return translation[:-2]
+
+
+def getParentsInYellowBox(soup : bs):
+    parent = soup.find_all(lambda tag: tag.has_attr("class") and tag["class"] == ["wpZ6GBd8"])
+    parents = ""
+    for x in parent:
+        try:
+            parents += (x.text) + ", "
+        except IndexError:
+            print("Error in finding parents")
+
+    return parents[:-2]
 
 
 def count_csv_rows(filename):
